40f5b9272696434c2cc8e48113e793bebc499de4
Dylanmurzello <dylanmurzello@gmail.com>
2020-12-21 11:35:59 +0530

    Add files via upload

---
+++ part1/bot.py
@@ -0,0 +1,101 @@
+import praw 
+import time
+import pyfiglet
+import random 
+import requests
+import sys
+from sys import exit
+
+
+
+
+reddit = praw.Reddit(client_id="dcgLhcyktxRAuQ",
+                     client_secret="LDUddhtr_g_z6-m08D6icp0TCBhFyg",
+                     password="Alive-Database-9353",
+                     user_agent="posted by u/Alive-Database-9353",
+                     username="Alive-Database-9353")
+ 
+result = pyfiglet.figlet_format("Dylan OP") 
+print(result) 
+
+print("Starting Magic............")
+
+print(reddit.user.me())
+
+REDDIT_USERNAME=(reddit.user.me())
+
+response = requests.get("https://www.reddit.com/user/{}/about.json".format(REDDIT_USERNAME),  headers = {'User-agent': 'hiiii its {}'.format(REDDIT_USERNAME)}).json()
+if "error" in response:
+ if response["error"] == 404:
+      print("account {} is shadowbanned. poor bot :( shutting down the script...".format(REDDIT_USERNAME))
+      sys.exit()
+ else:
+      print(response)
+else:
+    print("{} is not shadowbanned! We think..".format(REDDIT_USERNAME))
+
+
+title = input("Enter an epic title 1: ")
+title2 = input("Enter an epic title 2: ") 
+title3 = input("Enter an epic title 3: ")
+
+url = input("Enter a sassy link 1: ")
+url2 = input("Enter a sassy link 2: ")
+url3 = input("Enter a sassy link 3: ")
+
+
+comment = input ("Enter your comment 1: ")
+comment2 = input ("Enter your comment 2: ") 
+comment3 = input ("Enter your comment 3: ")
+
+print("Reading reddit list")
+subredit_list = open("data.txt", "r")
+subreddits = subredit_list.read().split(',')
+
+for subreddit in subreddits:
+  try:
+    print(subreddit)
+    reddit.validate_on_submit = True
+    submission = reddit.subreddit(subreddit).submit(title,url=url)
+    com = "###{}".format(comment)
+    time.sleep(10)
+    submission.reply(com)
+    print ("done")
+  except Exception as err:
+    print("Exception for subreddit {}, {}".format(subreddit, err))
+  t= random.randint(480,500)
+  seconds = "Sleeping for {} seconds before proceeding".format(t)
+  print(seconds)
+  time.sleep(t)
+
+for subreddit in subreddits:
+  try:
+    print(subreddit)
+    reddit.validate_on_submit = True
+    submission = reddit.subreddit(subreddit).submit(title2,url=url2)
+    com = "###{}".format(comment2)
+    time.sleep(10)
+    submission.reply(com)
+    print ("done")
+  except Exception as err:
+    print("Exception for subreddit {}, {}".format(subreddit, err))
+  t= random.randint(450,500)
+  seconds = "Sleeping for {} seconds before proceeding".format(t)
+  print(seconds)
+  time.sleep(t)
+
+for subreddit in subreddits:
+  try:
+    print(subreddit)
+    reddit.validate_on_submit = True
+    submission = reddit.subreddit(subreddit).submit(title3,url=url3)
+    com = "###{}".format(comment3)
+    time.sleep(10)
+    submission.reply(com)
+    print ("done")
+  except Exception as err:
+    print("Exception for subreddit {}, {}".format(subreddit, err))
+  t= random.randint(480,520)
+  seconds = "Sleeping for {} seconds before proceeding".format(t)
+  print(seconds)
+  time.sleep(t)
