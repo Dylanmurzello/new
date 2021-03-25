21e42f135281459199d7889cbf47db170ebe69b9
Dylanmurzello <dylanmurzello@gmail.com>
2021-03-25 18:02:48 +0530

    Update bot.py

--- part2/bot.py
+++ part2/bot.py
@@ -6,14 +6,6 @@ import requests
 import sys
 from sys import exit
 
-
-
-
-reddit = praw.Reddit(client_id="WnYYSHjTjuRC3A",
-                     client_secret="aVoIaSlEfyGvjbYaULY1MtOiRB3XZA",
-                     password="That-Ad3121",
-                     user_agent="posted by u/That-Ad3121",
-                     username="That-Ad3121")
  
 result = pyfiglet.figlet_format("Dylan OP") 
 print(result) 
